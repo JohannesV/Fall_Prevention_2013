82a965ca2b6793c0550c7316c3e60ac23b8c5a57
johannes <johannes.vassdal@gmail.com>
2013-02-15 15:26:38 +0100

    Made menu options
    but they still need activities to start.

--- FallPrevention/src/no/ntnu/stud/fallprevention/MainScreen.java
+++ FallPrevention/src/no/ntnu/stud/fallprevention/MainScreen.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,7 +15,8 @@ public class MainScreen extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mainscreen);
 
-		// Extract the information contained in the intent that created this activity
+		// Extract the information contained in the intent that created this
+		// activity
 		Intent motherIntent = getIntent();
 		String name = motherIntent
 				.getStringExtra("no.ntnu.stud.fallprevention.MESSAGE");
@@ -29,6 +31,18 @@ public class MainScreen extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_mainscreen, menu);
 		return true;
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()){
+		case R.id.menu_statistics:
+			//TODO: start the statistics activity
+			break;
+		case R.id.menu_settings:
+			//TODO: start the setting activity, if we get one
+			break;
+		}
+		return false;
 	}
 
 	public void fireEvent(View view) {
