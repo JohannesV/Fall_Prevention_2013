170358e884d794e2ce10151ce382d17ecc16717a
Elias <elias.aamot@gmail.no>
2013-02-22 10:46:27 +0100

    Removed sensors from statistics screen

--- FallPrevention/src/no/ntnu/stud/fallprevention/Statistics.java
+++ FallPrevention/src/no/ntnu/stud/fallprevention/Statistics.java
@@ -1,23 +1,15 @@
 package no.ntnu.stud.fallprevention;
 
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
-import android.widget.Toast;
 
-public class Statistics extends Activity implements SensorEventListener {
-
-	private SensorManager sm;
+public class Statistics extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_statistics);
-		sm = (SensorManager) getSystemService(SENSOR_SERVICE);
 	}
 
 	@Override
@@ -26,30 +18,5 @@ public class Statistics extends Activity implements SensorEventListener {
 		getMenuInflater().inflate(R.menu.activity_statistics, menu);
 		return true;
 	}
-	
-	@Override
-	protected void onPause() {
-		super.onPause();
-		sm.unregisterListener(this);
-	}
-	
-	@Override 
-	protected void onResume() {
-		super.onResume();
-		sm.registerListener(this, sm.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
-	}
-
-	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-		
-	}
-
-	@Override
-	public void onSensorChanged(SensorEvent event) {
-		float NS2S = 1.0f / 1000000000.0f;
-		
-		Toast.makeText(this, event.toString(), Toast.LENGTH_SHORT).show();
-	}
-	
 
 }
