e9dad60bfe33afb7082b8059d493ba63c3fa7979
dot <icanspell2dude@gmail.com>
2013-02-26 17:05:18 +0100

    test

---
+++ FallPrevention/src/no/ntnu/stud/fallprevention/ContactPerson.java
@@ -0,0 +1,17 @@
+package no.ntnu.stud.fallprevention;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class ContactPerson extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_related);
+	}
+
+}
