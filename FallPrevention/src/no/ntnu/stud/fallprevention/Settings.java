1a4649988972a9325fbed0ca5686391d9b68083e
Yunus Tayfun Kücükyareli <tayfun.kucukyareli@live.de>
2013-02-27 15:30:19 +0100

    Test

--- FallPrevention/src/no/ntnu/stud/fallprevention/Settings.java
+++ FallPrevention/src/no/ntnu/stud/fallprevention/Settings.java
@@ -1,8 +1,10 @@
 package no.ntnu.stud.fallprevention;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class Settings extends Activity {
@@ -11,7 +13,16 @@ public class Settings extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
+		final Button button = (Button) findViewById(R.id.nameButton);
+		
 	}
+	// testChange
+	void selfDestruct(){
+		Intent intent = new Intent(this, WriteName.class);
+		startActivity(intent);
+	}
+
+	
 
 	/*@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
