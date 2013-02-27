b70b811eacd26a45be4047247836af4f8a9bb646
vonbob <johannes.vassdal@gmail.com>
2013-02-27 21:03:39 +0100

    Made method in settings screen public, and changed .gitignore to what is reccommended for android development (Android.gitignore at Github)

--- FallPrevention/src/no/ntnu/stud/fallprevention/Settings.java
+++ FallPrevention/src/no/ntnu/stud/fallprevention/Settings.java
@@ -3,9 +3,8 @@ package no.ntnu.stud.fallprevention;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
 import android.widget.Button;
-import android.widget.Spinner;
 
 public class Settings extends Activity {
 
@@ -17,7 +16,7 @@ public class Settings extends Activity {
 		
 	}
 	// testChang55
-	void selfDestruct(){
+	public void selfDestruct(View v){
 		Intent intent = new Intent(this, WriteName.class);
 		startActivity(intent);
 	}
