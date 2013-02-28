0693089a564fdc3f2df4f0bb71de3b61b82c4505
dot <icanspell2dude@gmail.com>
2013-02-28 02:38:22 +0100

    Trying to fix the list button

--- FallPrevention/src/no/ntnu/stud/fallprevention/Related.java
+++ FallPrevention/src/no/ntnu/stud/fallprevention/Related.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,7 +20,15 @@ public class Related extends Activity {
 		//set inn getContactsfrom Database
 		contacts.add(new Contact("Tore", "Hansen", 42341515));
 		ListView listView = (ListView) findViewById(R.id.listView1);
-		
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {  
+			
+					@Override
+					public void onItemClick(AdapterView<?> arg0, View arg1,
+							int arg2, long arg3) {
+						// TODO Auto-generated method stub
+						fireEvent();
+					}  
+			      }); 
 		ArrayList<String> listItems = new ArrayList<String>();
 		for (Contact c : contacts) {
 			listItems.add(c.toString());
@@ -27,6 +38,11 @@ public class Related extends Activity {
 		
 		
 		
+	}
+	protected void fireEvent() {
+		// TODO Auto-generated method stub
+		Intent intent = new Intent(this, ContactPerson.class);
+		startActivity(intent);
 	}
 
 
