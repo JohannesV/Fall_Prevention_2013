a30dfd9d7f163862c40cd6eb7e67638eed23b864
Elias <elias.aamot@gmail.no>
2013-03-20 00:12:48 +0100

    Experimenting with smoothing and different peak value functions

--- Data/peak_algorithm.py
+++ Data/peak_algorithm.py
@@ -1,13 +1,13 @@
-import math, numpy
+import math, numpy, time
 
-def find_peaks(time_series=[], window=4, std_threshold=1.5):
+def find_peaks(time_series=[], window=4, std_threshold=1.0):
 
 	peak_function_values = [0]
 	peaks = []
 	
 	# Compute peak function
 	for i in xrange(1,len(time_series)-1,1):
-		value = s(window, i, time_series)
+		value = s2(window, i, time_series)
 		peak_function_values.append(value)
 	# Add dummy value to peak_function_values for the last element
 	peak_function_values.append(0)
@@ -38,7 +38,12 @@ def find_peaks(time_series=[], window=4, std_threshold=1.5):
 	
 	return peaks
 
-def s(window, i, time_series):
+# Dummy-function.
+def s0(window, i, time_series):
+	return time_series[i]
+
+# Max-calculation
+def s1(window, i, time_series):
 	before = get_slice(time_series, i-window, i)
 	diff1 = [time_series[i]-b for b in before]
 	maxdiff1 = max(diff1)
@@ -48,6 +53,45 @@ def s(window, i, time_series):
 	maxdiff2 = max(diff2)
 
 	return float(maxdiff1+maxdiff2)/2.0
+
+# Avg-calculation
+def s2(window, i, time_series):
+	before = get_slice(time_series, i-window, i)
+	diff1 = [time_series[i]-b for b in before]
+	sumdiff1 = sum(diff1)
+	kdiff1 = float(sumdiff1)/float(window)
+	
+	after = get_slice(time_series, i+1, i+window+1)
+	diff2 = [time_series[i]-a for a in after]
+	sumdiff2 = sum(diff2)
+	kdiff2 = float(sumdiff2)/float(window)
+
+	return float(kdiff1+kdiff2)/2.0
+
+# MaxEnt-calculation of 
+def s3(window, i, 
+time_series):
+	N = get_slice(time_series, i-window, i)+get_slice(time_series, i+1, i+window+1)
+	Nbar = get_slice(time_series, i-window, i+window+1)
+	return 0.0 - (entropy(N) - entropy(Nbar))
+
+KERNEL_WIDTH = 5
+
+def entropy(list):
+	return sum([((0.0-prw(i,list))*math.log(prw(i,list))) for i in xrange(len(list)-KERNEL_WIDTH)])
+
+# The Parzen-Rosenblatt window estimate of probability density
+def prw(i, peak_func):
+	if peak_func[i] - peak_func[i+KERNEL_WIDTH] != 0:
+		prefix = (1.0 / len(peak_func)*math.fabs(peak_func[i] - peak_func[i+KERNEL_WIDTH]))
+		suffix = sum([gauss_kernel((peak_func[i]-peak_func[j])/math.fabs(peak_func[i]-peak_func[i+KERNEL_WIDTH])) for j in xrange(len(peak_func))])
+		return prefix*suffix
+	else:
+		return 1.0
+
+
+def gauss_kernel(x):
+	return (1.0/math.sqrt(2.0*math.pi)) * math.exp((-1.0/2.0)*(x**2))
 
 def get_slice(list, start, end):
 	# Slice is entirely contained in list
@@ -62,3 +106,11 @@ def get_slice(list, start, end):
 	# Slice is larger than list in both directions
 	else:
 		return list[:]
+
+def smooth_moving(series, window):
+	smoothed_series = []
+	for i in xrange(len(series)):
+		window_series = get_slice(series, i-window, i+1+window)
+		smoothed = numpy.mean(window_series)
+		smoothed_series.append(smoothed)
+	return smoothed_series
--- Data/plot_acc_vector.py
+++ Data/plot_acc_vector.py
@@ -24,11 +24,19 @@ for i in xrange(len(a_z_data)):
 	length = math.sqrt(a_x_data[i]**2 + a_z_data[i]**2 + a_y_data[i]**2)
 	vector.append(length)
 
-peaks = pa.find_peaks(time_series=vector)
-
-for peak in peaks:
-	plt.vlines(peak, min(vector), max(vector), color='r')
-
 plt.plot(vector)
+s_vector_1 = pa.smooth_moving(vector, 1)
+#plt.plot(s_vector_1)
+s_vector_2 = pa.smooth_moving(vector, 2)
+#plt.plot(s_vector_2)
+s_vector_3 = pa.smooth_moving(vector, 3)
+plt.plot(s_vector_3)
+
+#peaks = pa.find_peaks(time_series=s_vector_3)
+
+#for peak in peaks:
+#	plt.vlines(peak, min(vector), max(vector), color='r')
+
+
 plt.title('Accelerometer Vector Length')
 plt.show()
