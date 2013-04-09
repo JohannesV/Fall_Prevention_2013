40ee0a29f6bb43423969188e3a083e505e5b801d
fiLLLip <filiptomren@gmail.com>
2013-04-09 13:24:58 +0200

    Fixed AndroidManifest. Solves Issue #5

--- ValensContentProvider/src/ntnu/stud/valens/contentprovider/ValensDataProvider.java
+++ ValensContentProvider/src/ntnu/stud/valens/contentprovider/ValensDataProvider.java
@@ -8,7 +8,6 @@ import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.provider.BaseColumns;
 import android.text.TextUtils;
