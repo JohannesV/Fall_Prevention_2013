9c362034ec3a7180834db026a8a67dec0f7e9f42
Elias <elias.aamot@gmail.no>
2013-04-17 23:32:03 +0200

    Some refactoring of source code placement, and work on research chapter

---
+++ FallPrevention/src/no/ntnu/stud/fallprevention/listadapters/EventListAdapter.java
@@ -0,0 +1,69 @@
+package no.ntnu.stud.fallprevention.listadapters;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import no.ntnu.stud.fallprevention.R;
+import no.ntnu.stud.fallprevention.datastructures.Event;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ *  Adapter for the class EventList. Gets the view of the list.
+ * @author Tayfun
+ *
+ */
+public class EventListAdapter extends ArrayAdapter<String> {
+
+	Context context;
+	List<Event> events;
+	/**
+	 * 
+	 * @param events: List for accessing the events
+	 * @return the new version of the list.
+	 */
+	private static List<String> eventToStringList(List<Event> events) {
+		List<String> strings = new ArrayList<String>();
+		for (Event e : events) {
+			strings.add(e.getTitle());
+		}
+		return strings;
+	}
+
+	public EventListAdapter(Context context, List<Event> events) {
+		super(context, R.layout.entry_eventlist, eventToStringList(events));
+		
+		this.context = context;
+		this.events = events;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		LayoutInflater inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+		View rowView = inflater.inflate(R.layout.entry_eventlist, parent, false);
+		TextView textView = (TextView) rowView.findViewById(R.id.label);
+		ImageView imageView = (ImageView) rowView.findViewById(R.id.logo);
+		textView.setText(events.get(position).getTitle());
+
+		// Set the correct icon
+		String s = events.get(position).getIcon();
+
+		if (s.equals("halo")) {
+			imageView.setImageResource(R.drawable.halo);
+		} else if (s.equals("sleep")) {
+			imageView.setImageResource(R.drawable.sleep);
+		} else {
+			imageView.setImageResource(R.drawable.smiley_icon);
+		}
+
+		return rowView;
+	}
+}
