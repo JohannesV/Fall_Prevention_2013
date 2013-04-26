c768048b552d62a12ddaa340e2dfbd62a335ffd4
Elias <elias.aamot@gmail.no>
2013-04-26 16:28:05 +0200

    Widget now updates regularly (30 min) without a timer, and onClick works

--- FallPrevention/src/no/ntnu/stud/fallprevention/widget/WidgetProvider.java
+++ FallPrevention/src/no/ntnu/stud/fallprevention/widget/WidgetProvider.java
@@ -1,9 +1,12 @@
 package no.ntnu.stud.fallprevention.widget;
 
 import no.ntnu.stud.fallprevention.R;
+import no.ntnu.stud.fallprevention.activity.EventList;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
@@ -16,7 +19,7 @@ import android.widget.Toast;
  */
 public class WidgetProvider extends AppWidgetProvider {
 
-	private int counter = 0;
+	private int counter;
 
 	/**
 	 * Updates the widgets through their widgets Ids
@@ -31,19 +34,18 @@ public class WidgetProvider extends AppWidgetProvider {
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 			int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
-		counter++;
+		counter = (int) (Math.random() * 100);
+		// Display text
 		Toast.makeText(context, "Counter: " + counter, Toast.LENGTH_LONG)
 				.show();
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout_no_messages);
         views.setTextViewText(R.id.textView1, "Counter: " + counter);
+        // Add onClick listener
+		Intent clickIntent = new Intent(context, EventList.class);
+		PendingIntent pendIntent = PendingIntent.getActivity(context,
+				0, clickIntent, 0);
+		views.setOnClickPendingIntent(R.id.smileyButton, pendIntent);
+		// Display changes
         appWidgetManager.updateAppWidget(appWidgetIds, views);
-
-		// declarates a new instance of the actual content has to been updated
-		// Intent intent = new Intent(context.getApplicationContext(),
-		// WidgetUpdateService.class);
-		// intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
-		// starts the update on the intent has to been updated
-		// Log.v("Widget", "Starting the service");
-		// context.startService(intent);
 	}
 }
