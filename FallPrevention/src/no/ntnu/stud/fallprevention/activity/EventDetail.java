248e8aca35f892811089038e2f27db18c34f71e1
Johannes <johannes.vassdal@gmail.com>
2013-05-02 16:07:10 +0200

    And fixing my merge-mistakes

--- FallPrevention/src/no/ntnu/stud/fallprevention/activity/EventDetail.java
+++ FallPrevention/src/no/ntnu/stud/fallprevention/activity/EventDetail.java
@@ -38,6 +38,7 @@ public class EventDetail extends Activity {
         textView.setText(eventInformation.get(DatabaseContract.EventType.COLUMN_NAME_TITLE));
         TextView textView2 = (TextView) findViewById(R.id.mainTextView);
         String mTempDesc = getString(R.string.event_desc_two_params);
+        
         mTempDesc=mTempDesc.replaceAll("%1", eventInformation.get(DatabaseContract.Event.COLUMN_NAME_PARAMETER_1));
         mTempDesc=mTempDesc.replaceAll("%2", eventInformation.get(DatabaseContract.Event.COLUMN_NAME_PARAMETER_2));
         mTempDesc=mTempDesc.replaceAll("null", getString(R.string.event_null_parameter));
