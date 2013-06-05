45518785845ab554a52ad117110183803aca02b0
Johannes <johannes.vassdal@gmail.com>
2013-06-05 09:43:03 +0200

    Added license headers to source code

---
+++ FallPrevention/gen/ntnu/stud/valens/demonstration/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ntnu.stud.valens.demonstration;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ FallTimeSeries/gen/ntnu/stud/valens/stepdetector/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ntnu.stud.valens.stepdetector;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
